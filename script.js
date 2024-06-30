e5a1e574e8d4644d0c3184bc0fa8c1cea72f3ef1
kol myers <118282955+empomz@users.noreply.github.com>
2024-06-30 13:26:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const btn = document.getElementById('button');
-
-document.getElementById('form')
-    .addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        btn.value = 'Sending...';
-
-        const serviceID = 'service_14qpfib';
-        const templateID = 'template_8a16mts';
-
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Send Email';
-                alert('Sent!');
-            }, (err) => {
-                btn.value = 'Send Email';
-                alert(JSON.stringify(err));
-            });
+(function (){ // Function to initialize the SDK with my pub key
+    emailjs.init({
+        publicKey: "LLYuY_o0nMHMlKO8W",
     });
+})
+();
+
+    const btn = document.getElementById('button');
+
+    document.getElementById('form')
+    .
+    addEventListener('submit', function (event) { //Listens to the form for it's information
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_8a16mts';
+
+    emailjs.sendForm(serviceID, templateID, this) // Sends the contact form to my email
+    .then(() => {
+    btn.value = 'Send Email';
+    alert('Sent!');
+}, (err) => {
+    btn.value = 'Send Email';
+    alert(JSON.stringify(err));
+});
+})
+;
